771e8a587faf313e7c3e6412b9bcc7f0a1147192
Marten Gülink <marten@ungeordnet.de>
2025-08-03 01:27:01 +0200

    bump package version to 0.0.4

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * Taggy SDK - A TypeScript SDK for the Taggy API
  * @module TaggySDK
  */
-export const VERSION = '0.0.3';
+export const VERSION = '0.0.4';
 
 // Export main client
 export { TaggyClient } from './client';
